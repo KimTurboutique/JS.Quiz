5b501b9a4af8383b64e31d2aac20317fb8b2c801
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-04-23 22:56:26 -0400

    updated html js and css added comments

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// step 1. start timer when user clicks start button
+
+function startTimer() {
+    alert("Quiz will start now")
+}
+
+var buttonElement=document.querySelector("button");
+buttonElement.addEventListener("click",startTimer);
