d2342b0eb554228ad0be86d287c679d4317e49d2
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-04-25 22:32:20 -0400

    updated html sections and added functions to script

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,97 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+        title: 'Commonly used data types DO NOT include:',
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts',
+    },
+    {
+        title: 'The condition in an if / else statement is enclosed within ____.',
+        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+        answer: 'parentheses',
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store ____.',
+        choices: [
+            'numbers and strings',
+            'other arrays',
+            'booleans',
+            'all of the above',
+        ],
+        answer: 'all of the above',
+    },
+    {
+        title:
+            'String values must be enclosed within ____ when being assigned to variables.',
+        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        answer: 'quotes',
+    },
+    {
+        title:
+            'A very useful tool used during development and debugging for printing content to the debugger is:',
+        choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+        answer: 'console.log',
+    },
+];
+
+// general
+
+var timeInterval;
+var counter = 60;
+var timeEl = document.getElementById("time");
+var currentIndex = 0;
+
 // step 1. start timer when user clicks start button
 
 function startTimer() {
-    alert("Quiz will start now")
+    timeEl.textContent = counter
+    timeInterval = setInterval(function () {
+        counter--
+        timeEl.textContent = counter
+        if (counter <= 0) {
+            endQuiz()
+        }
+    }, 1000)
+
 }
 
-var buttonElement=document.querySelector("button");
-buttonElement.addEventListener("click",startTimer);
+
+// Step 2. start quiz and display questions
+
+function startQuiz() {
+    document.getElementById("startScreen").classList.add("hide")
+    document.getElementById("questions").classList.remove("hide")
+    startTimer()
+    displayQuestions()
+}
+
+function displayQuestions() {
+    document.getElementById("question-title").textContent = questions[currentIndex].title
+    document.getElementById("answers").innerHTML = ""
+    for (var i = 0; i < questions[currentIndex].choices.length; i++) {
+        var btn = document.createElement("button")
+        btn.textContent = questions[currentIndex].choices[i]
+        btn.onclick = checkAnswer
+        document.getElementById("answers").append(btn)
+    }
+}
+
+// step 3. display answers and deduct 10 secs if user gets answer incorrect
+function checkAnswer(event) {
+    if (event.target.textContent === questions[currentIndex].answer) {
+
+    }
+    else {
+        counter -= 10
+        timeEl.textContent = counter
+    }
+}
+    
+// step 4. end quiz
+
+    function endQuiz() {
+        clearInterval(timeInterval)
+    }
+
+    var buttonElement = document.querySelector("#start-btn");
+    buttonElement.addEventListener("click", startQuiz);
